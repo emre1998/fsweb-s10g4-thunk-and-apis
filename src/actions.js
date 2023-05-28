a5fd092ed8615964fca4ba91a486098c484f3ff6
Emre Can Kızılateş <kizilatesemrecan@gmail.com>
2023-05-28 22:07:22 +0300

    S10G4 is all done

--- src/actions.js
+++ src/actions.js
@@ -1,9 +1,12 @@
+import axios from "axios";
+
 export const GET_FAVS_FROM_LS = "GET_FAVS_FROM_LS";
 export const FAV_ADD = "FAV_ADD";
 export const FAV_REMOVE = "FAV_REMOVE";
 export const FETCH_SUCCESS = "FETCH_SUCCESS";
 export const FETCH_LOADING = "FETCH_LOADING";
 export const FETCH_ERROR = "FETCH_ERROR";
+export const DELETE_ALL_FAV_FROM_STORAGE = "DELETE_ALL_FAV_FROM_STORAGE";
 
 export const getFavsFromLocalStorage = () => {
   return { type: GET_FAVS_FROM_LS }
@@ -17,5 +20,29 @@ export const removeFav = (id) => {
   return { type: FAV_REMOVE, payload: id }
 }
 
-export const fetchAnother = () => dispatch => {
+export const fetchSuccess = (data) => {
+  return {type:FETCH_SUCCESS, payload: data }
+}
+
+export const fetchError = (error) =>{
+  return {type:FETCH_ERROR, payload: error }
+}
+
+export const deleteAllFavs = (error) =>{
+  return {type:DELETE_ALL_FAV_FROM_STORAGE }
+}
+
+
+export const fetchAnother = () => (dispatch)  => {
+  dispatch({type:FETCH_LOADING})
+  return  axios
+    .get("https://official-joke-api.appspot.com/random_joke")
+    .then((response) => {
+      console.log("response.data", response.data);
+      dispatch(fetchSuccess(response.data))
+      
+    })
+    .catch((err) => {
+      dispatch(fetchError(err))
+    });
 }
--- src/reducers.js
+++ src/reducers.js
@@ -5,6 +5,7 @@ import {
   FETCH_LOADING,
   FETCH_ERROR,
   GET_FAVS_FROM_LS,
+  DELETE_ALL_FAV_FROM_STORAGE,
 } from "./actions";
 
 const initial = {
@@ -22,25 +23,61 @@ function readFavsFromLocalStorage() {
   return JSON.parse(localStorage.getItem("s10g4"));
 }
 
+function clearFavsFromLocalStorage(){
+  return localStorage.clear();
+}
+
 export function myReducer(state = initial, action) {
   switch (action.type) {
     case FAV_ADD:
-      return state;
+    const favAddValue = {
+      ...state,
+      favs : state.favs.find(item => item.id===action.payload.id)?state.favs:[...state.favs,action.payload]
+    }
+      writeFavsToLocalStorage(favAddValue);
+      return favAddValue;
 
     case FAV_REMOVE:
-      return state;
+      const favRemoveValue ={
+        ...state,
+        favs: state.favs.filter(item=> item.id!==action.payload),
+      };
+      writeFavsToLocalStorage(favRemoveValue);
+      return favRemoveValue;
 
     case FETCH_SUCCESS:
-      return state;
+      return {
+        ...state,
+        current : action.payload,
+        loading :false,
+      };
 
     case FETCH_LOADING:
-      return state;
+      return {
+        ...state,
+        loading :true,
+      };
 
     case FETCH_ERROR:
-      return state;
+      return {
+        ...state,
+        error : action.payload.message,
+        loading :false,
+      };
+
 
     case GET_FAVS_FROM_LS:
-      return state;
+      return {
+        ...state,
+        favs:readFavsFromLocalStorage()||initial.favs,
+
+      };
+
+    case DELETE_ALL_FAV_FROM_STORAGE:
+      clearFavsFromLocalStorage();
+    return  { ...state,
+    favs:initial.favs
+    };
 
     default:
       return state;
